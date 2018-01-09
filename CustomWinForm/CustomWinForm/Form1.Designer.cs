ef49c53ea4f3e0fede00e19f1ea1cc7673506264
Geovanny <vgfialloc@hotmail.com>
2018-01-09 09:01:54 -0500

    Add files via upload
    Formularios windows personalizados

---
+++ CustomWinForm/CustomWinForm/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace CustomWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TopBorderPanel = new System.Windows.Forms.Panel();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.LeftPanel = new System.Windows.Forms.Panel();
+            this.RightPanel = new System.Windows.Forms.Panel();
+            this.BottomPanel = new System.Windows.Forms.Panel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this._MaxButton = new CustomWinForm.MinMaxButton();
+            this._MinButton = new CustomWinForm.ButtonZ();
+            this._CloseButton = new CustomWinForm.ButtonZ();
+            this.TopPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopBorderPanel
+            // 
+            this.TopBorderPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
+            this.TopBorderPanel.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.TopBorderPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopBorderPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopBorderPanel.Name = "TopBorderPanel";
+            this.TopBorderPanel.Size = new System.Drawing.Size(684, 2);
+            this.TopBorderPanel.TabIndex = 0;
+            this.TopBorderPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopBorderPanel_MouseDown);
+            this.TopBorderPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopBorderPanel_MouseMove);
+            this.TopBorderPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopBorderPanel_MouseUp);
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this.TopPanel.Controls.Add(this._MaxButton);
+            this.TopPanel.Controls.Add(this._MinButton);
+            this.TopPanel.Controls.Add(this._CloseButton);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 2);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(684, 35);
+            this.TopPanel.TabIndex = 1;
+            this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.TopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            this.TopPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseUp);
+            // 
+            // LeftPanel
+            // 
+            this.LeftPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
+            this.LeftPanel.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftPanel.Location = new System.Drawing.Point(0, 37);
+            this.LeftPanel.Name = "LeftPanel";
+            this.LeftPanel.Size = new System.Drawing.Size(2, 424);
+            this.LeftPanel.TabIndex = 2;
+            this.LeftPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftPanel_MouseDown);
+            this.LeftPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftPanel_MouseMove);
+            this.LeftPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LeftPanel_MouseUp);
+            // 
+            // RightPanel
+            // 
+            this.RightPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
+            this.RightPanel.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.RightPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightPanel.Location = new System.Drawing.Point(682, 37);
+            this.RightPanel.Name = "RightPanel";
+            this.RightPanel.Size = new System.Drawing.Size(2, 424);
+            this.RightPanel.TabIndex = 3;
+            this.RightPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.RightPanel_MouseDown);
+            this.RightPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.RightPanel_MouseMove);
+            this.RightPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.RightPanel_MouseUp);
+            // 
+            // BottomPanel
+            // 
+            this.BottomPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
+            this.BottomPanel.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.BottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomPanel.Location = new System.Drawing.Point(2, 459);
+            this.BottomPanel.Name = "BottomPanel";
+            this.BottomPanel.Size = new System.Drawing.Size(680, 2);
+            this.BottomPanel.TabIndex = 4;
+            this.BottomPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BottomPanel_MouseDown);
+            this.BottomPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BottomPanel_MouseMove);
+            this.BottomPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.BottomPanel_MouseUp);
+            // 
+            // _MaxButton
+            // 
+            this._MaxButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._MaxButton.BZBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this._MaxButton.CFormState = CustomWinForm.MinMaxButton.CustomFormState.Normal;
+            this._MaxButton.DisplayText = "_";
+            this._MaxButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._MaxButton.ForeColor = System.Drawing.Color.White;
+            this._MaxButton.Location = new System.Drawing.Point(604, 3);
+            this._MaxButton.MouseClickColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(60)))));
+            this._MaxButton.MouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(80)))), ((int)(((byte)(180)))));
+            this._MaxButton.Name = "_MaxButton";
+            this._MaxButton.Size = new System.Drawing.Size(35, 25);
+            this._MaxButton.TabIndex = 2;
+            this._MaxButton.Text = "minMaxButton1";
+            this._MaxButton.TextLocation_X = 11;
+            this._MaxButton.TextLocation_Y = 8;
+            this.toolTip1.SetToolTip(this._MaxButton, "Maximize");
+            this._MaxButton.UseVisualStyleBackColor = true;
+            this._MaxButton.Click += new System.EventHandler(this._MaxButton_Click);
+            // 
+            // _MinButton
+            // 
+            this._MinButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._MinButton.BZBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this._MinButton.DisplayText = "_";
+            this._MinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._MinButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._MinButton.ForeColor = System.Drawing.Color.White;
+            this._MinButton.Location = new System.Drawing.Point(569, 3);
+            this._MinButton.MouseClickColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(60)))));
+            this._MinButton.MouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(80)))), ((int)(((byte)(180)))));
+            this._MinButton.Name = "_MinButton";
+            this._MinButton.Size = new System.Drawing.Size(35, 25);
+            this._MinButton.TabIndex = 1;
+            this._MinButton.Text = "_";
+            this._MinButton.TextLocation_X = 8;
+            this._MinButton.TextLocation_Y = -14;
+            this.toolTip1.SetToolTip(this._MinButton, "Minimize");
+            this._MinButton.UseVisualStyleBackColor = true;
+            this._MinButton.Click += new System.EventHandler(this._MinButton_Click);
+            // 
+            // _CloseButton
+            // 
+            this._CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._CloseButton.BZBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this._CloseButton.DisplayText = "X";
+            this._CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._CloseButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._CloseButton.ForeColor = System.Drawing.Color.White;
+            this._CloseButton.Location = new System.Drawing.Point(647, 3);
+            this._CloseButton.MouseClickColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this._CloseButton.MouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(80)))), ((int)(((byte)(180)))));
+            this._CloseButton.Name = "_CloseButton";
+            this._CloseButton.Size = new System.Drawing.Size(35, 25);
+            this._CloseButton.TabIndex = 0;
+            this._CloseButton.Text = "X";
+            this._CloseButton.TextLocation_X = 10;
+            this._CloseButton.TextLocation_Y = 4;
+            this.toolTip1.SetToolTip(this._CloseButton, "Close");
+            this._CloseButton.UseVisualStyleBackColor = true;
+            this._CloseButton.Click += new System.EventHandler(this._CloseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(60)))), ((int)(((byte)(150)))));
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.ControlBox = false;
+            this.Controls.Add(this.BottomPanel);
+            this.Controls.Add(this.RightPanel);
+            this.Controls.Add(this.LeftPanel);
+            this.Controls.Add(this.TopPanel);
+            this.Controls.Add(this.TopBorderPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopBorderPanel;
+        private System.Windows.Forms.Panel TopPanel;
+        private MinMaxButton _MaxButton;
+        private ButtonZ _MinButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private ButtonZ _CloseButton;
+        private System.Windows.Forms.Panel LeftPanel;
+        private System.Windows.Forms.Panel RightPanel;
+        private System.Windows.Forms.Panel BottomPanel;
+    }
+}
+
---
+++ CustomWinForm/CustomWinForm/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomWinForm
+{
+    public partial class Form1 : Form
+    {
+        bool isTopFormDragged = false;
+        bool isTopBorderDragged = false;
+        bool isLeftBorderDragged = false;
+        bool isRightBorderDragged = false;
+        bool isBottomBorderDragged = false;
+        bool isMaximized = false;
+        Point offset;
+        Size _normalSize;
+        Point _normalLocation = Point.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TopBorderPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                isTopBorderDragged = true;
+            else
+                isTopBorderDragged = false;
+        }
+
+        private void TopBorderPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Y < this.Location.Y)
+                if (isTopBorderDragged)
+                    if (this.Height < 50)
+                    {
+                        this.Height = 50;
+                        isTopBorderDragged = false;
+                    }
+                    else
+                    {
+                        this.Location = new Point(this.Location.X, this.Location.Y + e.Y);
+                        this.Height -= e.Y;
+                    }
+        }
+
+        private void TopBorderPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isTopBorderDragged = false;
+        }
+
+        private void TopPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isTopFormDragged = true;
+                Point pointStartPosition = this.PointToScreen(new Point(e.X, e.Y));
+                offset = new Point(this.Location.X - pointStartPosition.X, this.Location.Y - pointStartPosition.Y);
+            }
+            else
+                isTopFormDragged = false;
+            if(e.Clicks==2)
+            {
+                isTopFormDragged = false;
+                _MaxButton_Click(sender, e);
+            }
+        }
+
+        private void TopPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(isTopFormDragged)
+            {
+                Point newPoint = TopPanel.PointToScreen(new Point(e.X, e.Y));
+                newPoint.Offset(offset);
+                this.Location = newPoint;
+                if(this.Location.X>2||this.Location.Y>2)
+                    if(this.WindowState==FormWindowState.Maximized)
+                    {
+                        this.Location = _normalLocation;
+                        this.Size = _normalSize;
+                        toolTip1.SetToolTip(_MaxButton, "Maximize");
+                        _MaxButton.CFormState = MinMaxButton.CustomFormState.Normal;
+                        isMaximized = false;
+                    }
+            }
+        }
+
+        private void TopPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isTopFormDragged = false;
+            if(this.Location.Y<=5)
+                if(!isMaximized)
+                {
+                    _normalSize = this.Size;
+                    _normalLocation = this.Location;
+                    Rectangle rect = Screen.PrimaryScreen.WorkingArea;
+                    this.Location = new Point(0, 0);
+                    this.Size = new Size(rect.Width, rect.Height);
+                    toolTip1.SetToolTip(_MaxButton, "Restore Down");
+                    _MaxButton.CFormState = MinMaxButton.CustomFormState.Maximize;
+                    isMaximized = true;
+                }
+        }
+
+        private void LeftPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (this.Location.X <= 0 || e.X < 0)
+            {
+                isLeftBorderDragged = false;
+                this.Location = new Point(10, this.Location.Y);
+            }
+            else
+                if (e.Button == MouseButtons.Left)
+                {
+                    isLeftBorderDragged = true;
+                }
+                else
+                {
+                    isLeftBorderDragged = false;
+                }
+        }
+
+        private void LeftPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.X < this.Location.X)
+                if (isLeftBorderDragged)
+                {
+                    if (this.Width < 100)
+                    {
+                        this.Width = 100;
+                        isLeftBorderDragged = false;
+                    }
+                    else
+                    {
+                        this.Location = new Point(this.Location.X + e.X, this.Location.Y);
+                        this.Width = this.Width - e.X;
+                    }
+                }
+        }
+
+        private void LeftPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isLeftBorderDragged = false;
+        }
+
+        private void RightPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                isRightBorderDragged = true;
+            else
+                isRightBorderDragged = false;
+        }
+
+        private void RightPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isRightBorderDragged)
+                if (this.Width < 100)
+                {
+                    this.Width = 100;
+                    isRightBorderDragged = false;
+                }
+                else
+                    this.Width = this.Width + e.X;
+        }
+
+        private void RightPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isRightBorderDragged = false;
+        }
+
+        private void BottomPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                isBottomBorderDragged = true;
+            else
+                isBottomBorderDragged = false;
+        }
+
+        private void BottomPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isBottomBorderDragged)
+            {
+                if (this.Height < 50)
+                {
+                    this.Height = 50;
+                    isBottomBorderDragged = false;
+                }
+                else
+                    this.Height = this.Height + e.Y;
+            }
+        }
+
+        private void BottomPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isBottomBorderDragged = false;
+        }
+
+        private void _MinButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void _MaxButton_Click(object sender, EventArgs e)
+        {
+            if (isMaximized)
+            {
+                this.Location = _normalLocation;
+                this.Size = _normalSize;
+                toolTip1.SetToolTip(_MaxButton, "Maximize");
+                _MaxButton.CFormState = MinMaxButton.CustomFormState.Normal;
+                isMaximized = false;
+            }
+            else
+            {
+                _normalSize = this.Size;
+                _normalLocation = this.Location;
+
+                Rectangle rect = Screen.PrimaryScreen.WorkingArea;
+                this.Location = new Point(0, 0);
+                this.Size = new Size(rect.Width, rect.Height);
+                toolTip1.SetToolTip(_MaxButton, "Restore Down");
+                _MaxButton.CFormState = MinMaxButton.CustomFormState.Maximize;
+                isMaximized = true;
+            }
+        }
+
+        private void _CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
